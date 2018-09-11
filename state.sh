22928a0a4105c15e17813d96424dbca15ce27d36
Flysky12138 <35610243+Flysky12138@users.noreply.github.com>
2018-09-11 09:51:18 +0800

    Update state.sh

--- state.sh
+++ state.sh
@@ -16,21 +16,14 @@ for x in `$box cat /data/misc/net/rt_tables`;do
 done
 TX=`echo "${SWK#*TX bytes:}" | $box sed 's/\([^(]*(\)\([^)]*\)\(.*\)/\2/;s/i.*//g'`
 RX=`echo "${SWK#*RX bytes:}" | $box sed 's/\([^(]*(\)\([^)]*\)\(.*\)/\2/;s/i.*//g'`
-#↓热点流量：上传/下载
-source /data/misc/wifi/hostapd.conf >/dev/null 2>&1
-RWK=`$box ifconfig $interface` >/dev/null 2>&1
-TX1=`echo ${RWK#*TX bytes} | $box cut -d "(" -f 2 | $box cut -d ")" -f 1 | $box cut -d "i" -f 1`
-RX1=`echo ${RWK#*RX bytes} | $box cut -d "(" -f 2 | $box cut -d ")" -f 1 | $box cut -d "i" -f 1`
 #↓显示输出
 echo ""
 for x in redsocks2 gost pdnsd ss-local;do
     [ "`$box pgrep $x`" != "" ] && A=" ✔" || A=" ✘"
-    [ "$x" = "gost" -a "$TX" != "" ] && B="▲$TX" 
-    [ "$x" = "gost" -a "$RX1" != "0.0 B" ] && C="▲$TX1" 
+    [ "$x" = "gost" -a "$TX" != "" ] && B="▲$TX"
     [ "$x" = "pdnsd" -a "$RX" != "" ] && B="▼$RX"
-    [ "$x" = "pdnsd" -a "$RX1" != "0.0 B" ] && C="▼$RX1"
-    printf "%-5s%-13s%-15s%s\n" "$A" "$x" "$B" "$C"
-    A="";B="";C=""
+    printf "%-5s%-13s%s\n" "$A" "$x" "$B"
+    A="";B=""
 done
 echo ""
 echo "→ nat ←"
